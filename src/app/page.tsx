2520e2ac9e102075f49b743c79d85c0447ae0dcc
wahyuucandra <wahyuucandra@gmail.com>
2024-07-11 14:24:06 +0700

    pretest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,66 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 
 export interface Todo {
   id: string;
   name: string;
-  checkbox: boolean
+  checkbox: boolean;
 }
 
 export default function Home() {
   const [toDo, setToDo] = useState({
     id: "",
     name: "",
-    checkbox: false
+    checkbox: false,
   });
-  const [toDoList, setToDoList] = useState<Todo[]>();
+  const [toDoList, setToDoList] = useState<Todo[]>([]);
 
-  const addToDoList= () => {
-    //setToDoList({
-    //})
-  }
+  const addToDoList = () => {
+    setToDoList([...toDoList, toDo]);
+    setToDo({
+      id: "",
+      name: "",
+      checkbox: false,
+    })
+  };
 
   const setOnchangeId = (e: any) => {
     setToDo({
       ...toDo,
-      id: e.target.value
-    })
-  }
+      id: e.target.value,
+    });
+  };
 
   const setOnchangeName = (e: any) => {
     setToDo({
       ...toDo,
-      name: e.target.value
-    })
-  }
+      name: e.target.value,
+    });
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-row gap-24">
         <div id="add-todo-list">
-          <form onSubmit={addToDoList} className="flex flex-col gap-8">
-            
+          <form className="flex flex-col gap-8">
             <label htmlFor="id">ID</label>
-            <input className="text-black" value={toDo?.id} type="number" onChange={ e => setOnchangeId(e)}/>
+            <input
+              className="text-black"
+              value={toDo?.id}
+              type="number"
+              onChange={(e) => setOnchangeId(e)}
+            />
 
             <label htmlFor="nama">Nama</label>
-            <input className="text-black" value={toDo?.name} type="text" onChange={ e => setOnchangeName(e)}/>
+            <input
+              className="text-black"
+              value={toDo?.name}
+              type="text"
+              onChange={(e) => setOnchangeName(e)}
+            />
 
-            <button type="submit">Simpan</button>
+            <button className="rounded-lg py-3 px-2 bg-white text-black" type="button" onClick={() => addToDoList()}>Simpan</button>
           </form>
         </div>
 
@@ -56,8 +69,10 @@ export default function Home() {
           {toDoList?.map((item) => {
             return (
               <div key={item.id}>
-                <input type="checkbox" value={item.id}/>
-                <p>{item.name}</p>
+                <div className="flex gap-2">
+                  <input type="checkbox" value={item.id} />
+                  <p>{item.name}</p>
+                </div>
               </div>
             );
           })}
